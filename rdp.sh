e5360518d827891f5149637532c2bdc6f7bcad77
Dr.FarFar دكتور فرفر <38743218+3XS0@users.noreply.github.com>
2021-08-20 14:06:17 +0200

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP To Your System [ WwW.Dr-FarFar.CoM ]" >&2
 {
-sudo useradd -m LPTECH
-sudo adduser LPTECH sudo
-echo 'LPTECH:8426' | sudo chpasswd
+sudo useradd -m DrFarFar
+sudo adduser DrFarFar sudo
+echo 'DrFarFar:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser LPTECH chrome-remote-desktop
+sudo adduser DrFarFar chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete [ WwW.Dr-FarFar.CoM ]" >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - LPTECH -c """$CRP"""
+su - DrFarFar -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
